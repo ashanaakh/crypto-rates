e558567c7d17902e747867bd289cfa6b2c0f9c28
Ali Shanaakh <ashanaakh@gmail.com>
2018-02-07 18:12:29 +0200

    Add logic and fixes

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fatih/color"
+	"http"
+	"io/ioutil"
+	"ioutil"
+	"os"
 )
 
+func url(base string) string {
+	return "https://chasing-coins.com/api/v1/std/coin/" + base
+}
+
+func printCurrenciesRates() {
+	coins := []string{"BTC", "ETH", "XRP"}
+
+	for _, coin := range coins {
+		response, err := http.Get(url(coin))
+
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		} else {
+			defer response.Body.Close()
+			contents, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				fmt.Printf("%s", err)
+				os.Exit(1)
+			}
+			color.Green(string(contents))
+		}
+
+	}
+}
+
 func main() {
-	color := color.New(color.FgCyan)
-	color.Println("Colorful output")
+	printCurrenciesRates()
 }
