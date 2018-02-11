a555b10be9ef054e107ec457bf2a50319a27a278
Ali Shanaakh <ashanaakh@gmail.com>
2018-02-12 01:41:08 +0200

    Refactor main.go

--- main.go
+++ main.go
@@ -6,72 +6,8 @@
 
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strconv"
-
-	"github.com/fatih/color"
-)
-
-// List of supported currencies
-var coins = []string{"BTC", "ETH", "XRP"}
-
-// cryptoCurrencyChange
-type cryptoCurrencyChange struct {
-	Hour string `json:"hour"`
-	Day  string `json:"day"`
-}
-
-// CryptoCurrency for api resp
-type cryptoCurrency struct {
-	Price  string               `json:"price"`
-	Change cryptoCurrencyChange `json:"change"`
-}
-
-func getCoinURL(base string) string {
-	return "https://chasing-coins.com/api/v1/std/coin/" + base
-}
-
-func printCurrenciesRates() {
-	for _, coin := range coins {
-
-		url := getCoinURL(coin)
-
-		res, err := http.Get(url)
-
-		if err != nil {
-			fmt.Printf("%s", err)
-			os.Exit(1)
-		}
-
-		body, err := ioutil.ReadAll(res.Body)
-
-		if err != nil {
-			fmt.Printf("%s", err)
-			os.Exit(1)
-		}
-
-		crypt := new(cryptoCurrency)
-		json.Unmarshal([]byte(body), &crypt)
-
-		boldYellow := color.New(color.FgYellow).Add(color.Bold)
-		boldYellow.Print(coin + " ")
-
-		hour, _ := strconv.ParseFloat(crypt.Change.Hour, 64)
-
-		if hour > 0 {
-			color.Green(crypt.Price)
-		} else {
-			color.Red(crypt.Price)
-		}
-
-	}
-}
+import "github.com/ashanaakh/crypto-rates/command"
 
 func main() {
-	printCurrenciesRates()
+	command.Run()
 }
